f037cdf91deb000ebe5896277796d96ea1876d48
david <bolarinwa29@gmail.com>
2023-03-24 02:19:25 +0000

    my first try

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash/
+#author       :bolarinwa david wealth
+#trademark    :david
+#function     :this script will add users and assign password
+#special      :you can aslo rerun automatically***
+
+#start
+echo "welcome $USER, this script is designed to setup your system with nano, tree,
+
+#end
